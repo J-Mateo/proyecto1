72a046f282eade2cf753f88a6076c71fc0b9d0b0
J-Mateo <jessica.mateo.fraile@gmail.com>
2026-04-18 01:21:59 +0200

    cambios sprint 5

---
+++ js/data/resources.js
@@ -0,0 +1,61 @@
+export const resources = [
+    {
+        category: "Documentación Oficial",
+        items: [
+            { title: "MDN Web Docs", url: "https://developer.mozilla.org/", note: "La biblia del desarrollo web." },
+            { title: "W3C Specifications", url: "https://www.w3.org/TR/", note: "Estándares oficiales de la web." },
+            { title: "Can I Use", url: "https://caniuse.com/", note: "Tablas de compatibilidad de navegadores." },
+            { title: "DevDocs", url: "https://devdocs.io/", note: "Documentaciones combinadas en un solo lugar." },
+            { title: "Form Submit", url: "https://formsubmit.co/", note: "Herramienta para formularios fácil." }
+        ]
+    },
+    {
+        category: "Aprendizaje",
+        items: [
+            { title: "freeCodeCamp", url: "https://www.freecodecamp.org/", note: "Cursos y certificaciones gratuitas." },
+            { title: "JavaScript.info", url: "https://javascript.info/", note: "El mejor tutorial profundo de JS." },
+            { title: "CSS-Tricks", url: "https://css-tricks.com/", note: "Artículos técnicos excelentes sobre CSS." },
+            { title: "Web.dev", url: "https://web.dev/", note: "Guías de Google para el desarrollo moderno." },
+            { title: "Frontend Masters", url: "https://frontendmasters.com/", note: "Cursos avanzados de gran calidad." }
+        ]
+    },
+    {
+        category: "Herramientas",
+        items: [
+            { title: "VS Code", url: "https://code.visualstudio.com/", note: "El editor de código estándar actual." },
+            { title: "GitHub", url: "https://github.com/", note: "Hosting para control de versiones Git." },
+            { title: "CodePen", url: "https://codepen.io/", note: "Entorno para pruebas rápidas de frontend." },
+            { title: "Figma", url: "https://www.figma.com/", note: "Diseño colaborativo de interfaces." },
+            { title: "Netlify", url: "https://www.netlify.com/", note: "Despliegue rápido de sitios estáticos." }
+        ]
+    },
+    {
+        category: "Accesibilidad",
+        items: [
+            { title: "WCAG Guidelines", url: "https://www.w3.org/WAI/standards-guidelines/wcag/", note: "Las pautas oficiales de accesibilidad." },
+            { title: "WebAIM", url: "https://webaim.org/", note: "Recursos y tutoriales de accesibilidad web." },
+            { title: "WAVE Tool", url: "https://wave.webaim.org/", note: "Evaluador visual de accesibilidad." },
+            { title: "Axe DevTools", url: "https://www.deque.com/axe/", note: "Extensión para auditar código." }
+        ]
+    },
+    {
+        category: "Blogs & Comunidades",
+        items: [
+            { title: "Smashing Magazine", url: "https://www.smashingmagazine.com/", note: "Artículos sobre diseño y desarrollo." },
+            { title: "A List Apart", url: "https://alistapart.com/", note: "Explora el diseño y desarrollo web." },
+            { title: "Dev.to", url: "https://dev.to/", note: "Comunidad abierta de programadores." },
+            { title: "Stack Overflow", url: "https://stackoverflow.com/", note: "Resolución de dudas técnicas." },
+            { title: "Frontend Focus", url: "https://frontendfoc.us/", note: "Newsletter semanal de frontend." }
+        ]
+    },
+    {
+        category: "Inspiración & Design",
+        items: [
+            { title: "Awwwards", url: "https://www.awwwards.com/", note: "Premios a los mejores sitios web." },
+            { title: "Dribbble", url: "https://dribbble.com/", note: "Showcase para diseñadores." },
+            { title: "Behance", url: "https://www.behance.net/", note: "Portafolios creativos de todo tipo." },
+            { title: "Coolors", url: "https://coolors.co/", note: "Generador de paletas de colores." },
+            { title: "Google Fonts", url: "https://fonts.google.com/", note: "Librería gratuita de tipografías web." }
+        ]
+    }
+];
